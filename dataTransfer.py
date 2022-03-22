0c7919a5a0107b2e9d6a369ef83b6da5878eef67
Joe <Joe.zhang229@gmail.com>
2022-03-22 02:22:54 -0700

    export data in list

--- dataTransfer.py
+++ dataTransfer.py
@@ -2,27 +2,37 @@ import csv
 import xml.etree.ElementTree as ET
 
 
-def importData(file1, *args, **kwargs):
-    # with open(file1, 'r') as file1:
-    #     file1_read = csv.reader(file1)
+def importData(filePath, dataList, results=[]):
+    for dataType in dataList:
+        results.append(dataHelper(filePath, dataType))
+    return results
+
+
+def dataHelper(file, dataType):
+    results = []
+    # with open(file, 'r') as file:
+    #     file1_read = csv.reader(file)
 
     #     for line in file1_read:
     # elementTree obj
-    tree = ET.parse(file1)
+    tree = ET.parse(file)
     root = tree.getroot()
-    for facility in root.findall('T_Facility'):
-        # print(ET.tostring(facility))
-        facilityId = facility.find('Facility_ID').text
-        print(facilityId)
-
-# root=tree2.getroot()
-# for element in root.findall('Facility_ID'):
-#     id = element.text
-#     print(id)
-# for facility in root.findall('T_Facility'):
-# print(ET.tostring(facility))
-# facilityId=facility.find('Facility_ID').text
-# print(facility)
+    facilities = root.findall('T_Facility')
+    for facility in facilities:
+        # grabs facility data based on type
+        facilityData = facility.find(dataType)
+        if facilityData is None:
+            continue
+        else:
+            # print(facilityData.text)
+            results.append(facilityData.text)
+    return results
 
 
-print(importData('./XC2 Export Data/Facility.xml'))
+dict = ['Facility_ID', 'Facility_Account_Number', 'Facility_Name']
+
+# print(dataHelper('./XC2 Export Data/Facility.xml', 'Facility_ID'))
+# print(dataHelper('./XC2 Export Data/Facility.xml', 'Facility_Account_Number'))
+# print('Names', dataHelper('./XC2 Export Data/Facility.xml', 'Facility_Name'))
+# print(importData('./XC2 Export Data/Facility.xml', dict))
+print(importData('./XC2 Export Data/Facility.xml', dict))
